daaeb438718b68d58112722c24002250379cbc12
qipan <qipan940>
2021-05-13 23:09:54 +0800

    first commit

---
+++ mylibrary/build/generated/source/buildConfig/debug/com/example/myrobotlibrary/BuildConfig.java
@@ -0,0 +1,19 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.myrobotlibrary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.example.myrobotlibrary";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "0.1";
+  // Fields from build type: debug
+  public static final String ANDROID_EMULATOR_LOCALHOST = "api.cmind-medical.com";
+  public static final int PORT = 9003;
+  public static final String SP_PAD_SIGN = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDdaUpFV7DCyA17LgFJN/M2oZCAcnQSVgda+m02pQUyMn70xb2TDxLQ3z5G5UqLF2IoDP7yvxv4ozLb4QzkB7NPP8tmjJyMJmiuH5BiYawm1ga4YHy5yVul+YeuVbqWEmwlfuOBnV6L7Q7ftOnYLp8IRjzJFjTJcx4v9Bf+x16K2QIDAQAB";
+  public static final String baseHttpUrl = "http://api.cmind-medical.com/api";
+  public static final String baseMqttUrl = "tcp://8.135.38.211:1883";
+  public static final boolean isDebug = true;
+}
---
+++ mylibrary/build/generated/source/buildConfig/release/com/example/myrobotlibrary/BuildConfig.java
@@ -0,0 +1,19 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.myrobotlibrary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.example.myrobotlibrary";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "0.1";
+  // Fields from build type: release
+  public static final String ANDROID_EMULATOR_LOCALHOST = "logistics-api.epsit.cn";
+  public static final int PORT = 9003;
+  public static final String SP_PAD_SIGN = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDdaUpFV7DCyA17LgFJN/M2oZCAcnQSVgda+m02pQUyMn70xb2TDxv4ozLb4QzkB7NPP8tmjJyMJmiuH5BiYawm1ga4YHy5yVulEissieuVbqWEmwlfuOBnV6L7Q7ftOnYLp8IRjzJFjTJcx4v9Bf+x16K2QPRO";
+  public static final String baseHttpUrl = "https://logistics-api.epsit.cn/api";
+  public static final String baseMqttUrl = "tcp://logistics-api.epsit.cn:1883";
+  public static final boolean isDebug = false;
+}
